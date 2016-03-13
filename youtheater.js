bde94832e91983bee1d21094edb64d160bbcb90a
Stepan Kuntco aka StepLg <steplg@gmail.com>
2016-03-13 15:49:10 -0700

    fix js path, save quality in cookies

--- youtheater.js
+++ youtheater.js
@@ -30,9 +30,12 @@ function startVideo(url) {
   player1 = new YT.Player('player1', {
     width: $(window).width(),
     height: $(window).height(),
+    // suggestedQuality: 'hd1080',
     videoId: vid,
+
     events: {
       'onReady': function(event) {onPlayerReady(event, url)},
+      'onPlaybackQualityChange': onPlaybackQualityChange,
     }
   });
 }
@@ -50,11 +53,23 @@ function onPlayerReady(event, url) {
   t = t != null ? t : $.url('#t', url);
   event.target.seekTo(parseTime(t), true);
   event.target.playVideo();
+  defaultQuality = $.cookie('DefaultQuality');
+  if (defaultQuality == null) {
+    defaultQuality = 'hd1080';
+  }
+
+  event.target.setPlaybackQuality(defaultQuality);
+}
+
+function onPlaybackQualityChange(event) {
+  console.log("changing quality to " + event.target.getPlaybackQuality());
+  $.cookie('DefaultQuality', event.target.getPlaybackQuality(), { expires: 7, path: '/' });
+  console.log($.cookie('DefaultQuality'));
 }
 
 $(document).ready(function() {
   $( "#yurl" ).focus();
-  
+
   $( "#f1" ).submit(function( event ) {
     yurl = $( "#yurl" ).val();
     startVideo(yurl)
